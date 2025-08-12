4ad0a982558ede6da3ca29af63c2132d2f2f334f
Leo Policastro <43428385+leopolicastro@users.noreply.github.com>
2025-08-12 08:45:25 -0400

    update theme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,8 +30,8 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  colorscheme = "tokyonight", -- Add this line
+  -- install = { colorscheme = { "tokyonight", "habamax" } },
+  -- colorscheme = "tokyonight", -- Add this line
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
---
+++ lua/plugins/themes.lua
@@ -0,0 +1,61 @@
+-- return {
+--   {
+--     "folke/tokyonight.nvim",
+--     name = "tokyonight",
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "tokyonight-storm",
+--     },
+--   },
+-- }
+--
+--
+-- return {
+--   "ribru17/bamboo.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("bamboo").setup({})
+--     require("bamboo").load()
+--   end,
+-- }
+--
+--
+return {
+  { "EdenEast/nightfox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordfox",
+    },
+  },
+}
+--
+--
+-- return {
+--   { "rebelot/kanagawa.nvim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "kanagawa",
+--     },
+--   },
+-- }
+--
+--
+-- return {
+--   {
+--     "folke/tokyonight.nvim",
+--     name = "tokyonight",
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "tokyonight-storm",
+--     },
+--   },
+-- }
+--
+--
